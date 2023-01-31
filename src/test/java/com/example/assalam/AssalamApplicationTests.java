2cc5defa1c5af9136b6af1de71346e2f2ef7c8a0
mohamedHcodes <alisanmolhamed@gmail.com>
2023-01-31 11:32:01 +0300

    here is my spring framework commit

---
+++ src/test/java/com/example/assalam/AssalamApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.assalam;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AssalamApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
